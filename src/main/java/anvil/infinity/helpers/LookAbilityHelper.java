95ca7f1519b76101c610fa4b58e9473e795267db
Timo Ley <tilera.mpz@gmail.com>
2019-05-20 18:13:26 +0200

    addition

--- src/main/java/anvil/infinity/helpers/LookAbilityHelper.java
+++ src/main/java/anvil/infinity/helpers/LookAbilityHelper.java
@@ -1,4 +1,21 @@
 package anvil.infinity.helpers;
 
-public class LokkAbilityHelper {
+import anvil.infinity.registry.Effects;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.PotionEffect;
+
+public class LookAbilityHelper {
+    public static final int KILL = 0;
+
+    public static void activateEntityLookAbility(Entity entity, int abilityID) {
+        switch (abilityID) {
+            case KILL: if (entity instanceof EntityLivingBase) {
+                ((EntityLivingBase) entity).addPotionEffect(new PotionEffect(Effects.snapEffect, 1));
+            } break;
+        }
+    }
+
+
+
 }
